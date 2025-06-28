5fc7151900ac6de89c3dbae3d92b88642d373d92
David Nguyen <h250694@gmail.com>
2025-06-28 12:47:57 +0700

    Enhance StaticPageContent styles: remove bottom padding and improve layout for better presentation Add responsive styles for code and pre elements in prose to ensure proper display on smaller screens

--- packages/blog-starter-kit/themes/eplus/components/static-page-content.tsx
+++ packages/blog-starter-kit/themes/eplus/components/static-page-content.tsx
@@ -8,7 +8,7 @@ function StaticPageContent(props: Props) {
   const { content, title } = props.pageContent;
 
   return (
-    <div className={`blog-page-card pb-32`}>
+    <div className={`blog-page-card`}>
       <div
         className={`blog-page-content prose prose-lg mx-auto break-words tracking-tight dark:prose-dark xl:prose-xl`}
       >
