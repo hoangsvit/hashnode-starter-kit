048ca8a5b864f59ec12873ebf736d5fdebefeb7c
David Nguyen <h250694@gmail.com>
2025-06-28 17:20:50 +0700

    Add nextjs-toploader dependency and integrate it into the app for improved loading experience

--- packages/blog-starter-kit/themes/eplus/pages/_app.tsx
+++ packages/blog-starter-kit/themes/eplus/pages/_app.tsx
@@ -2,7 +2,7 @@ import { withUrqlClient } from 'next-urql';
 import { AppProps } from 'next/app';
 import { useEffect } from 'react';
 import 'tailwindcss/tailwind.css';
-
+import NextTopLoader from 'nextjs-toploader';
 import { GlobalFontVariables } from '../components/fonts';
 import { getUrqlClientConfig } from '../lib/api/client';
 import '../styles/index.css';
@@ -21,6 +21,20 @@ function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<Fragment>
 			<GlobalFontVariables />
+			<NextTopLoader color="#2299DD"
+				initialPosition={0.08}
+				crawlSpeed={200}
+				height={3}
+				crawl={true}
+				showSpinner={true}
+				easing="ease"
+				speed={200}
+				shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+				template='<div class="bar" role="bar"><div class="peg"></div></div>
+				<div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+				zIndex={1600}
+				showAtBottom={false}
+			/>
 			<Component {...pageProps} />
 		</Fragment>
 	);
