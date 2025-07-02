33bb844ca91ba8bf0906651710afcf5d55124038
David <h250694@gmail.com>
2025-07-02 13:15:16 +0700

    feat: add responsive padding to Container component for improved layout

--- packages/blog-starter-kit/themes/eplus/pages/[slug].tsx
+++ packages/blog-starter-kit/themes/eplus/pages/[slug].tsx
@@ -190,7 +190,7 @@ export default function PostOrPage(props: Props) {
 					>
 						<PostPageNavbar publication={publication} ref={headerRef} />
 					</header>
-					<Container>
+					<Container className="px-4 md:px-6 lg:px-8">
 						<article className="flex flex-col items-start gap-10 pb-10">
 							<Post {...props} />
 						</article>
@@ -250,7 +250,7 @@ export default function PostOrPage(props: Props) {
 				>
 					<PostPageNavbar publication={publication} ref={headerRef} />
 				</header>
-				<Container>
+				<Container className="px-4 md:px-6 lg:px-8">
 					<article className="flex flex-col items-start gap-10 pb-10">
 						<Page {...props} />
 					</article>
