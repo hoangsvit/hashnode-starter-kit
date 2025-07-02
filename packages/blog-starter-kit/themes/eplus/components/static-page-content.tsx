b188b43485df109607c598e9e3a8abcb7f0df710
David <h250694@gmail.com>
2025-07-02 12:01:13 +0700

    feat: enhance static page content styles for improved responsiveness and accessibility

--- packages/blog-starter-kit/themes/eplus/components/static-page-content.tsx
+++ packages/blog-starter-kit/themes/eplus/components/static-page-content.tsx
@@ -8,9 +8,9 @@ function StaticPageContent(props: Props) {
   const { content, title } = props.pageContent;
 
   return (
-    <div className={`blog-page-card`}>
+    <div className={`blog-page-card pb-32`}>
       <div
-        className={`blog-page-content prose prose-lg mx-auto break-words tracking-tight dark:prose-dark xl:prose-xl`}
+        className={`blog-page-content prose prose-lg dark:prose-dark mx-auto break-words tracking-tight xl:prose-xl`}
       >
         <h1
           className={`blog-page-title mb-10 break-words text-3xl font-bold text-black dark:text-white md:text-4xl xl:text-5xl`}
@@ -18,6 +18,7 @@ function StaticPageContent(props: Props) {
           {title}
         </h1>
         <div
+          className="prose-content overflow-x-auto"
           dangerouslySetInnerHTML={{
             __html: content.html,
           }}
