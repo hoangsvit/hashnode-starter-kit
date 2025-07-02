68afefa3e17bd54eabe7b175f5f3b948242f25a3
Rcv! Nguyen Hoang <nguyen.minh.hoang@rivercrane.vn>
2025-07-02 10:48:19 +0700

    feat: enhance Google Analytics integration with new tracking utilities and improved configuration

--- packages/blog-starter-kit/themes/eplus/components/analytics.tsx
+++ packages/blog-starter-kit/themes/eplus/components/analytics.tsx
@@ -3,139 +3,150 @@ import { useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import { useAppContext } from './contexts/appContext';
-const GA_TRACKING_ID = 'G-NYCMVVREGV'; // This is Hashnode's GA tracking ID
 const isProd = process.env.NEXT_PUBLIC_MODE === 'production';
 const BASE_PATH = process.env.NEXT_PUBLIC_BASE_URL || '';
 
 export const Analytics = () => {
 	const { publication, post, series, page } = useAppContext();
-
-	const _sendPageViewsToHashnodeGoogleAnalytics = () => {
-		// @ts-ignore
-		window.gtag('config', GA_TRACKING_ID, {
-			transport_url: 'https://ping.hashnode.com',
-			first_party_collection: true,
-		});
-	};
-
-	const _sendViewsToHashnodeInternalAnalytics = async () => {
-		// Send to Hashnode's own internal analytics
-		const event: Record<string, string | number | object> = {
-			event_type: 'pageview',
-			time: new Date().getTime(),
-			event_properties: {
-				hostname: window.location.hostname,
-				url: window.location.pathname,
-				eventType: 'pageview',
-				publicationId: publication.id,
-				dateAdded: new Date().getTime(),
-				referrer: window.document.referrer,
-			},
-		};
-
-		let deviceId = Cookies.get('__amplitudeDeviceID');
-		if (!deviceId) {
-			deviceId = uuidv4();
-			Cookies.set('__amplitudeDeviceID', deviceId, {
-				expires: 365 * 2,
-			}); // expire after two years
-		}
-
-		event['device_id'] = deviceId;
-
-		await fetch(`${BASE_PATH}/ping/data-event`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({ events: [event] }),
-		});
-	};
-
-	function _sendViewsToAdvancedAnalyticsDashboard() {
-		const publicationId = publication.id;
-		const postId = post && post.id;
-		const seriesId = series?.id || post?.series?.id;
-		const staticPageId = page && page.id;
-
-		const data = {
-			publicationId,
-			postId,
-			seriesId,
-			staticPageId,
-		};
-
-		if (!publicationId) {
-			console.warn('Publication ID is missing; could not send analytics.');
-			return;
-		}
-
-		const isBrowser = typeof window !== 'undefined';
-		if (!isBrowser) {
-			return;
-		}
-
-		const isLocalhost = window.location.hostname === 'localhost';
-		if (isLocalhost) {
-			console.warn(
-				'Analytics API call is skipped because you are running on localhost; data:',
-				data,
-			);
-			return;
-		}
-
-		const event = {
-			// timestamp will be added in API
-			payload: {
-				publicationId,
-				postId: postId || null,
-				seriesId: seriesId || null,
-				pageId: staticPageId || null,
-				url: window.location.href,
-				referrer: document.referrer || null,
-				language: navigator.language || null,
-				screen: `${window.screen.width}x${window.screen.height}`,
-			},
-			type: 'pageview',
-		};
-
-		const blob = new Blob(
-			[
-				JSON.stringify({
-					events: [event],
-				}),
-			],
-			{
-				type: 'application/json; charset=UTF-8',
-			},
-		);
-
-		let hasSentBeacon = false;
-		try {
-			if (navigator.sendBeacon) {
-				hasSentBeacon = navigator.sendBeacon(`${BASE_PATH}/api/analytics`, blob);
-			}
-		} catch (error) {
-			// do nothing; in case there is an error we fall back to fetch
-		}
-
-		if (!hasSentBeacon) {
-			fetch(`${BASE_PATH}/api/analytics`, {
-				method: 'POST',
-				body: blob,
-				credentials: 'omit',
-				keepalive: true,
-			});
-		}
-	}
+	const gaTrackingID = publication.integrations?.gaTrackingID || process.env.NEXT_PUBLIC_GA_TRACKING_ID;
 
 	useEffect(() => {
 		if (!isProd) return;
 
-		_sendPageViewsToHashnodeGoogleAnalytics();
+		const _sendPageViewsToGoogleAnalytics = () => {
+			if (!gaTrackingID || typeof window === 'undefined') return;
+			
+			// @ts-ignore
+			if (typeof window.gtag !== 'undefined') {
+				// @ts-ignore
+				window.gtag('config', gaTrackingID, {
+					page_title: document.title,
+					page_location: window.location.href,
+				});
+				// @ts-ignore
+				window.gtag('event', 'page_view', {
+					page_title: document.title,
+					page_location: window.location.href,
+					page_path: window.location.pathname,
+				});
+			}
+		};
+
+		const _sendViewsToHashnodeInternalAnalytics = async () => {
+			// Send to Hashnode's own internal analytics
+			const event: Record<string, string | number | object> = {
+				event_type: 'pageview',
+				time: new Date().getTime(),
+				event_properties: {
+					hostname: window.location.hostname,
+					url: window.location.pathname,
+					eventType: 'pageview',
+					publicationId: publication.id,
+					dateAdded: new Date().getTime(),
+					referrer: window.document.referrer,
+				},
+			};
+
+			let deviceId = Cookies.get('__amplitudeDeviceID');
+			if (!deviceId) {
+				deviceId = uuidv4();
+				Cookies.set('__amplitudeDeviceID', deviceId, {
+					expires: 365 * 2,
+				}); // expire after two years
+			}
+
+			event['device_id'] = deviceId;
+
+			await fetch(`${BASE_PATH}/ping/data-event`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ events: [event] }),
+			});
+		};
+
+		const _sendViewsToAdvancedAnalyticsDashboard = () => {
+			const publicationId = publication.id;
+			const postId = post?.id;
+			const seriesId = series?.id || post?.series?.id;
+			const staticPageId = page?.id;
+
+			const data = {
+				publicationId,
+				postId,
+				seriesId,
+				staticPageId,
+			};
+
+			if (!publicationId) {
+				console.warn('Publication ID is missing; could not send analytics.');
+				return;
+			}
+
+			const isBrowser = typeof window !== 'undefined';
+			if (!isBrowser) {
+				return;
+			}
+
+			const isLocalhost = window.location.hostname === 'localhost';
+			if (isLocalhost) {
+				console.warn(
+					'Analytics API call is skipped because you are running on localhost; data:',
+					data,
+				);
+				return;
+			}
+
+			const event = {
+				// timestamp will be added in API
+				payload: {
+					publicationId,
+					postId: postId || null,
+					seriesId: seriesId || null,
+					pageId: staticPageId || null,
+					url: window.location.href,
+					referrer: document.referrer || null,
+					language: navigator.language || null,
+					screen: `${window.screen.width}x${window.screen.height}`,
+				},
+				type: 'pageview',
+			};
+
+			const blob = new Blob(
+				[
+					JSON.stringify({
+						events: [event],
+					}),
+				],
+				{
+					type: 'application/json; charset=UTF-8',
+				},
+			);
+
+			let hasSentBeacon = false;
+			try {
+				if (navigator.sendBeacon) {
+					hasSentBeacon = navigator.sendBeacon(`${BASE_PATH}/api/analytics`, blob);
+				}
+			} catch (error) {
+				console.warn('SendBeacon failed, falling back to fetch:', error);
+			}
+
+			if (!hasSentBeacon) {
+				fetch(`${BASE_PATH}/api/analytics`, {
+					method: 'POST',
+					body: blob,
+					credentials: 'omit',
+					keepalive: true,
+				});
+			}
+		};
+
+		_sendPageViewsToGoogleAnalytics();
 		_sendViewsToHashnodeInternalAnalytics();
 		_sendViewsToAdvancedAnalyticsDashboard();
-	}, []);
+	}, [gaTrackingID, publication.id, post?.id, post?.series?.id, series?.id, page?.id]);
 
 	return null;
 };
