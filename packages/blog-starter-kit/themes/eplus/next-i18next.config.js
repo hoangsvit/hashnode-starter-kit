b74ab8770f10ff06426ea9eb52904ef132ed2158
David <h250694@gmail.com>
2025-07-04 09:59:05 +0700

    Disables locale detection
    Disables locale detection in the i18n configuration.
    
    This change prevents automatic redirection based on the user's browser settings, allowing for a more controlled and predictable user experience.

--- packages/blog-starter-kit/themes/eplus/next-i18next.config.js
+++ packages/blog-starter-kit/themes/eplus/next-i18next.config.js
@@ -3,8 +3,8 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'vi'],
-    localePath: './public/locales',
-    localeDetection: true,
+    localeDetection: false,
   },
+  localePath: './public/locales',
   reloadOnPrerender: process.env.NODE_ENV === 'development',
 }
