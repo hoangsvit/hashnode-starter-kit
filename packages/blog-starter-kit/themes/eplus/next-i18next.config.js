62387e53ab05b643278f9d09d5e0e64e54c82251
David Nguyen <h250694@gmail.com>
2025-07-04 18:37:41 +0700

    feat(i18n): disable locale detection in configuration

--- packages/blog-starter-kit/themes/eplus/next-i18next.config.js
+++ packages/blog-starter-kit/themes/eplus/next-i18next.config.js
@@ -3,8 +3,8 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'vi', 'ja', 'es', 'fr', 'zh', 'hi'],
-    localePath: './public/locales',
-    localeDetection: true,
+    localeDetection: false,
   },
+  localePath: './public/locales',
   reloadOnPrerender: process.env.NODE_ENV === 'development',
 }
