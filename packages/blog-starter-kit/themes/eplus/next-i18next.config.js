0025665b7b871ec866830d27e6f8c16f7496b4ce
David <h250694@gmail.com>
2025-07-04 10:04:12 +0700

    fix: enable locale detection in next-i18next configuration

--- packages/blog-starter-kit/themes/eplus/next-i18next.config.js
+++ packages/blog-starter-kit/themes/eplus/next-i18next.config.js
@@ -3,8 +3,8 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'vi'],
-    localeDetection: false,
+    localePath: './public/locales',
+    localeDetection: true,
   },
-  localePath: './public/locales',
   reloadOnPrerender: process.env.NODE_ENV === 'development',
 }
