fa5aafb061deedffb6bdaf6d795c97238b8de7e4
David Nguyen <h250694@gmail.com>
2025-07-05 08:24:40 +0700

    fix(i18n): remove trailingSlash option from next-i18next configuration

--- packages/blog-starter-kit/themes/eplus/next-i18next.config.js
+++ packages/blog-starter-kit/themes/eplus/next-i18next.config.js
@@ -4,7 +4,6 @@ module.exports = {
     defaultLocale: 'en',
     locales: ['en', 'vi', 'ja', 'es', 'fr', 'zh', 'hi'],
     localeDetection: false,
-    trailingSlash: false,
   },
   localePath: './public/locales',
   reloadOnPrerender: process.env.NODE_ENV === 'development',
