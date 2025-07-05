b577facd50aa9c2d3d96d531b6100f3999bc9548
David Nguyen <h250694@gmail.com>
2025-07-05 16:43:33 +0700

    refactor: convert ProfileImage to class component and improve structure

--- packages/blog-starter-kit/themes/eplus/components/profile-image.js
+++ packages/blog-starter-kit/themes/eplus/components/profile-image.js
@@ -1,47 +1,60 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { twMerge } from 'tailwind-merge';
 import Image from 'next/legacy/image';
 
 import { resizeImage } from '../utils/image';
 import { DEFAULT_AVATAR } from '../utils/const/images';
 
-const ProfileImage = ({ user, blogURL, postUrlForAnonymous, className, width, height }) => {
-  const profileImageRef = useRef(null);
+export default class ProfileImage extends React.Component {
+  componentDidMount() {
+    if (!this.props.user) {
+      return;
+    }
+    if (this.props.user.isDeactivated) {
+      return;
+    }
+  }
 
-  const href = blogURL
-    ? blogURL
-    : user && !user.isDeactivated
-    ? `https://hashnode.com/@${user.username}`
-    : postUrlForAnonymous
-    ? postUrlForAnonymous
-    : '#';
-
-  const imageWidth = width ? parseInt(width) : 70;
-  const imageHeight = height ? parseInt(height) : 70;
-
-  return (
-    <a
-      href={href}
-      ref={profileImageRef}
-      className={`relative block h-full w-full`}
-    >
-      <Image
-        className={twMerge(
-          className,
-          `relative z-20 block w-full rounded-full`,
-          process.env.NEXT_PUBLIC_BLUR_IMAGES === 'true' ? 'dev-mode-blur-image' : ''
-        )}
-        src={
-          user && user.profilePicture
-            ? resizeImage(user.profilePicture, { w: imageWidth, h: imageHeight, c: 'face' })
-            : DEFAULT_AVATAR
+  render() {
+    const user = this.props.user;
+    const blogURL = this.props.blogURL;
+    return (
+      <a
+        href={
+          blogURL
+            ? blogURL
+            : user && !user.isDeactivated
+            ? `https://hashnode.com/@${user.username}`
+            : this.props.postUrlForAnonymous
+            ? this.props.postUrlForAnonymous
+            : '#'
         }
-        width={imageWidth}
-        height={imageHeight}
-        alt={user ? user.name + "'s photo" : undefined}
-      />
-    </a>
-  );
-};
-
-export default ProfileImage;
+        ref={(c) => {
+          this.profileImage = c;
+        }}
+        className={`relative block h-full w-full`}
+      >
+        <Image
+          className={twMerge(
+            this.props.className,
+            `relative z-20 block w-full rounded-full`,
+            process.env.NEXT_PUBLIC_BLUR_IMAGES === 'true' ? 'dev-mode-blur-image' : ''
+          )}
+          src={
+            user && user.profilePicture
+              ? resizeImage(user.profilePicture, { w: this.props.width || 70, h: this.props.height || 70, c: 'face' })
+              : DEFAULT_AVATAR
+          }
+          width={this.props.width ? parseInt(this.props.width) : 70}
+          height={this.props.height ? parseInt(this.props.height) : 70}
+          // resize={{
+          //   w: this.props.width ? parseInt(this.props.width) : 70,
+          //   h: this.props.height ? parseInt(this.props.height) : 70,
+          //   c: 'face',
+          // }}
+          alt={user ? user.name + "'s photo" : undefined}
+        />
+      </a>
+    );
+  }
+}
